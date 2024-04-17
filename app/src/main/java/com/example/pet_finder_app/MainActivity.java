a02637c9dc8059210bc86269f8d2420d89a5ea8f
vvvjey <hoangtu4520031234@gmail.com>
2024-04-17 22:43:06 +0700

    Add firebase to project

--- app/src/main/java/com/example/pet_finder_app/MainActivity.java
+++ app/src/main/java/com/example/pet_finder_app/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.pet_finder_app;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
@@ -85,5 +92,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
     }
 }
