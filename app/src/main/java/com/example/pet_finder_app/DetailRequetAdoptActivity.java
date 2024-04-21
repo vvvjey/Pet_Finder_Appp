d51a48bf2a105ebd336472d3b618fea4827a9034
vvvjey <hoangtu4520031234@gmail.com>
2024-04-21 16:33:58 +0700

    Detail request,reject request dialog

---
+++ app/src/main/java/com/example/pet_finder_app/DetailRequetAdoptActivity.java
@@ -0,0 +1,48 @@
+package com.example.pet_finder_app;
+
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailRequetAdoptActivity extends AppCompatActivity {
+    Button rejectBtn;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.detail_request_adopt);
+        rejectBtn = findViewById(R.id.rejectBtn);
+        rejectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View dialogView = LayoutInflater.from(DetailRequetAdoptActivity.this).inflate(R.layout.detail_request_adopt_reject_dialog, null);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(DetailRequetAdoptActivity.this);
+                builder.setView(dialogView);
+
+                AlertDialog dialog = builder.create();
+
+                dialog.show();
+                TextView closeButton = dialogView.findViewById(R.id.closeRejectAadoptBtn);
+                closeButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Dismiss the dialog when the "Close" button is clicked
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+
+
+    }
+}
