41ee38ea8ff18b68c748fd9291847a725cddbbad
UIT-21522130 <96097433+UIT-21522130@users.noreply.github.com>
2024-05-12 22:18:42 +0700

    forget Password

--- app/src/main/java/com/example/pet_finder_app/LoginActivity.java
+++ app/src/main/java/com/example/pet_finder_app/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.pet_finder_app;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Patterns;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -32,7 +36,7 @@ import com.google.firebase.auth.GoogleAuthProvider;
 import javax.annotation.Nullable;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView toRegisterTv;
+    TextView toRegisterTv, forgetPassword;
     EditText email, password;
     private FirebaseAuth auth;
 
@@ -48,6 +52,7 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.edtEmail);
         auth = FirebaseAuth.getInstance();
         loginGGBtn = findViewById(R.id.btnLoginGG);
+        forgetPassword = findViewById(R.id.tvForgotPassword);
         // register
         toRegisterTv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,7 +70,7 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-
+        //----------Login with Google----------
         GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken("1:237004107904:android:e6bd3849ee129775027665")
                 .requestEmail()
@@ -76,6 +81,53 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i = client.getSignInIntent();
                 startActivityForResult(i, 1234);
+            }
+        });
+        //----------Forget Password----------
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                View dialogView = getLayoutInflater().inflate(R.layout.forget_password, null);
+                EditText emailBox = dialogView.findViewById(R.id.emailBox);
+
+                builder.setView(dialogView);
+                AlertDialog dialog = builder.create();
+
+                dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String userEmail = emailBox.getText().toString();
+
+                        if (TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+                            Toast.makeText(LoginActivity.this, "Enter your registered email!", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        auth.sendPasswordResetEmail(userEmail)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(LoginActivity.this, "Check your email!", Toast.LENGTH_SHORT).show();
+                                        dialog.dismiss();
+                                    }
+                                    else{
+                                        Toast.makeText(LoginActivity.this, "Failed to send!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                    }
+                });
+                dialogView.findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                if(dialog.getWindow() != null){
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                }
+                dialog.show();
             }
         });
 
@@ -126,13 +178,13 @@ public class LoginActivity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signInWithCredential(credential)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>(){
                     @Override
-                            public void onComplete (@NonNull Task<AuthResult> task){
-                                if (task.isSuccessful()){
-                                    startActivity(new Intent(LoginActivity.this, Splash1Activity.class));
-                                }
-                                else{
-                                    Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                }
+                    public void onComplete (@NonNull Task<AuthResult> task){
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(LoginActivity.this, Splash1Activity.class));
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
             } catch (ApiException e){
