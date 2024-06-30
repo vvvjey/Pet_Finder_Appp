6b7cd3eac7c31d4169763b7d54b6919503992817
UIT-21522029 <96015169+Napeno@users.noreply.github.com>
2024-06-30 19:23:18 +0700

    Phan chuyen thuat thuc

--- app/src/main/java/com/example/pet_finder_app/fragments/RequestAdopt.java
+++ app/src/main/java/com/example/pet_finder_app/fragments/RequestAdopt.java
@@ -29,6 +29,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -159,8 +160,7 @@ public class  RequestAdopt extends Fragment {
                 }
             }
         }
-
-
+        Collections.reverse(petItems);
         AdoptStatusAdapter petAdapter = new AdoptStatusAdapter(petItems, getContext());
         recyclerView.setAdapter(petAdapter);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 1));
